1b0d0efcbca71891320ccb4c46b2f958f4aa1cd9
TIANCHENG <txiong53@wisc.edu>
2022-08-01 15:43:37 -0500

    update

--- node/index.js
+++ node/index.js
@@ -3,6 +3,9 @@ const fs = require("fs");
 const http = require("http");
 const url = require("url");
 
+const slugify = require("slugify");
+const replaceTemplate = require("./modules/replaceTemp");
+
 // const hello = "hello world";
 // console.log(hello);
 // const { log } = require("console");
@@ -33,23 +36,6 @@ const url = require("url");
 
 //SERVER
 
-const replaceTemplate = (temp, product) => {
-  //regex expression, to replace all placeholders
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
-
-  if (!product.organic)
-    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-
-  return output;
-};
-
 //only execute once at the beginning, so it does not matter to use sync
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const tempOverview = fs.readFileSync(
@@ -67,9 +53,16 @@ const tempProduct = fs.readFileSync(
 
 const dataObj = JSON.parse(data);
 
+const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
+console.log(slugs);
+console.log(
+  slugify("Fresh Avocados", {
+    lower: true,
+  })
+);
+
 const server = http.createServer((req, res) => {
   //
-
   const { query, pathname } = url.parse(req.url, true);
   //
 
