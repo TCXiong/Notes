15f2a205761d0f03f8f85ddb1948a0a22d7501d9
TIANCHENG <txiong53@wisc.edu>
2022-08-27 23:26:49 -0500

    update

---
+++ natours/controllers/userController.js
@@ -0,0 +1,34 @@
+exports.getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: 'err',
+    message: 'Not defined',
+  });
+};
+
+exports.getUser = (req, res) => {
+  res.status(500).json({
+    status: 'err',
+    message: 'Not defined',
+  });
+};
+
+exports.createUser = (req, res) => {
+  res.status(500).json({
+    status: 'err',
+    message: 'Not defined',
+  });
+};
+
+exports.updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'err',
+    message: 'Not defined',
+  });
+};
+
+exports.deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'err',
+    message: 'Not defined',
+  });
+};
